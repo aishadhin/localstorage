7984ce3f08a86bdf0e6a6ae69d805603078edb78
Ai Shadhin <aishadhin.ac@gmail.com>
2022-05-10 17:52:04 +0600

    add multiple item with multiple quantity

--- src/component/AllUsers/AllUsers.js
+++ src/component/AllUsers/AllUsers.js
@@ -1,27 +1,37 @@
-import React from 'react';
-import './AllUsers.css'
+import React from "react";
+import "./AllUsers.css";
 
-const AllUsers = ({user}) => {
-    const {id, name, des} = user;
+const AllUsers = ({ user }) => {
+  const { id, name, des } = user;
 
-    const addToCartHandle = id => {
-        
-        if(localStorage.getItem(id)){
-            // const newQuantity = localStorage.getItem(id) + 1;
-            localStorage.setItem(id ,parseInt(localStorage.getItem(id)) + 1)
-        }else{
-            localStorage.setItem(id , 1);
-        }
+  const addToCartHandle = (id) => {
+    let obj = {};
+
+    const getObj = localStorage.getItem("cart-items");
+    if(getObj){
+        obj = JSON.parse(getObj)
+    }else{
+        obj = {}
     }
-    return (
-        <div className='user'>
-            <h3>id: {id}</h3>
-            <h2>name: {name}</h2>
-            <p>{des}</p>
-            <button onClick={() => addToCartHandle(id)}>Add to cart</button>
-            <button>Remove</button>
-        </div>
-    );
+
+    if (obj[id]) {
+      obj[id] = obj[id] + 1;
+    }else{
+        obj[id] = 1;
+    }
+
+    localStorage.setItem("cart-items", JSON.stringify(obj));
+  };
+
+  return (
+    <div className="user">
+      <h3>id: {id}</h3>
+      <h2>name: {name}</h2>
+      <p>{des}</p>
+      <button onClick={() => addToCartHandle(id)}>Add to cart</button>
+      <button>Remove</button>
+    </div>
+  );
 };
 
 export default AllUsers;
